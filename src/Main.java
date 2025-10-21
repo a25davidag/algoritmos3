3d55bcf8c6ab677708e8bf194b0bf532d4900022
David Arranz González <a25davidag@teis.intranet>
2025-10-21 17:37:06 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ejercicio1
+    public static double getSalaryMean(double[] salaires) {
+
+        return 0;
+    }
+
+//Ejercicio2
+    public static double getSalaryMedian(double[]salaries) {
+
+        return 0;
+    }
+//Ejercicio3
+        public static double getPayroll(double salary, int children,double percentage, boolean disability) {
+
+        return 0;
+        }
+//Ejercicio4
+    public static double[][] transposeMatrix(double[][] matrix) {
+
+        return matrix;
+
+    }
+
+
+}
